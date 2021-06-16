828455461205627deeb038d9ff5919945d076d1d
Matthew <mattj20@gmail.com>
2021-06-15 18:06:31 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+    counter = 2
+    if num > 1
+        range = (counter..num-1).to_a
+        range.none? do |number|
+        num % number == 0
+        end
+    else false   
+    end
+end
